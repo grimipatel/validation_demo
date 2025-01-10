644c92a5faf7be565a11d25ed6902622a96a1171
grimipatel <grimipatel@gmail.com>
2025-01-10 11:12:54 +0530

    practice reverse number

---
+++ js_pre.js
@@ -0,0 +1,7 @@
+const number =543
+const str = number.toString()
+const strArray = str.split('')
+const reverseArray =strArray.reverse()
+const arrayToString = reverseArray.join('')
+
+console.log("revrse is : ", Number(arrayToString))
