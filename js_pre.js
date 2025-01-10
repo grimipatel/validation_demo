d992abb5028c04af7858cd80f6f142acf67aa5f0
grimipatel <grimipatel@gmail.com>
2025-01-10 11:43:24 +0530

    switch statement

--- js_pre.js
+++ js_pre.js
@@ -4,12 +4,30 @@
 // const reverseArray =strArray.reverse()
 // const arrayToString = reverseArray.join('')
 
-// console.log("revrse is : ", Number(arrayToString))
+// console.log("reverse is : ", Number(arrayToString))
 
-let age = prompt("what is your age")
-if(age>10 && age<20){
-    console.log("your age lies between 10 and 20")
-}
-else{
-    console.log("your age doesn't lies between 10 and 20")
+// let age = prompt("what is your age")
+// if(age>10 && age<20){
+//     console.log("your age lies between 10 and 20")
+// }
+// else{
+//     console.log("your age doesn't lies between 10 and 20")
+// }
+
+let age = prompt("what is your age");
+switch(age){
+    case 12:
+        console.log("your age is 12")
+        break;
+    case 13:
+        console.log("your age is 12")
+        break;
+    case 14:
+        console.log("your age is 12")
+        break;
+    case 15:
+        console.log("your age is 12")
+        break;
+    default:
+        console.log("your age is not special")
 }
