d31c35190046592b33916403993fd34e8066ba8b
grimipatel <grimipatel@gmail.com>
2025-01-10 11:26:11 +0530

    practice if else statement

--- js_pre.js
+++ js_pre.js
@@ -1,7 +1,15 @@
-const number =543
-const str = number.toString()
-const strArray = str.split('')
-const reverseArray =strArray.reverse()
-const arrayToString = reverseArray.join('')
+// const number =543
+// const str = number.toString()
+// const strArray = str.split('')
+// const reverseArray =strArray.reverse()
+// const arrayToString = reverseArray.join('')
 
-console.log("revrse is : ", Number(arrayToString))
+// console.log("revrse is : ", Number(arrayToString))
+
+let age = prompt("what is your age")
+if(age>10 && age<20){
+    console.log("your age lies between 10 and 20")
+}
+else{
+    console.log("your age doesn't lies between 10 and 20")
+}
