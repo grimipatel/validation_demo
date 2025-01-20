d3f88d023c04a88e5c93e6007802665bc0bb02ec
grimipatel <grimipatel@gmail.com>
2025-01-20 14:45:44 +0530

    practice of precticeset question1 using for-in loop

--- js_pre.js
+++ js_pre.js
@@ -176,6 +176,7 @@ console.log(reverseNum(123))*/
         dhani : 37
     }
     
-    for (i = 0; i< Object.keys(marks).length; i++){
-        console.log(Object.keys(marks)[i] + marks[Object.keys(marks)[i]])
+    for (let i in marks){
+        console.log(i + marks [i])
     }
+
