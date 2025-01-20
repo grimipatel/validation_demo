21cc1e439b2e6fedb3fe9c3032b950a4eb6637c8
grimipatel <grimipatel@gmail.com>
2025-01-20 11:38:43 +0530

    practice of reverse number using while loop

--- js_pre.js
+++ js_pre.js
@@ -114,10 +114,24 @@ console.log(reverseNum(123))*/
      console.log(a + obj[a])
   }*/
 
-     let n = prompt("enter the value of n")
+     /*let n = prompt("enter the value of n")
      n = Number.parseInt(n)
      let i = 0;
      while(i < n){
         console.log(i)
         i++; 
-     }
+     }*/
+
+
+     function reverseNum(num){
+        if(num>=0 && num<=9){
+            return num;
+        }
+        let reverseNum = 0;
+        while(num != 0){
+            reverseNum = reverseNum * 10 +(num%10)
+            num = Math.floor(num/10)
+        }
+       return reverseNum;
+    }
+    console.log(reverseNum(123))
