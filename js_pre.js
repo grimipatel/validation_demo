89a21f13f2901177f8b8e02b4e0e19bae3780742
grimipatel <grimipatel@gmail.com>
2025-01-20 14:19:55 +0530

    practice of created basic functions

--- js_pre.js
+++ js_pre.js
@@ -144,14 +144,27 @@ console.log(reverseNum(123))*/
         i++;
     }while(i < n)*/
 
+
+    const hello = ()=>{
+        console.log("hey how are you")
+        return "hi"
+    }
+
     function onePlusAvg(x,y){
         return 1 + (x + y) / 2
+    }
+    
+    const sum = (p, q)=>{
+        return p + q
     }
 
     let a = 1;
     let b = 2;
     let c = 3;
+    let v = hello();
 
+    console.log(v)
     console.log(onePlusAvg(a ,b))
     console.log(onePlusAvg(b, c))
     console.log(onePlusAvg(c, a))
+    console.log(sum(9,7))
